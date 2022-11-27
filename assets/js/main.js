2dfa061f509c1cb3f79482c9a6d2cc2aebbf2b76
Muqadasa-Sherani <kawsar.moqadsa@gmail.com>
2022-11-27 16:57:59 +0300

    scroll up done

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,9 +143,27 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    //When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80){
+        nav.classList.add('scroll-header');
+    } else{
+        nav.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-up');
+    //when the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll
+    if(this.scrollY >= 560){
+        scrollTop.classList.add('show-scroll');
+    }else{
+        scrollTop.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
